dcc24ddd6f8f73cdeec26f7be8a46c4a400b6b40
lvd. <nbikiti@gmail.com>
2025-10-28 00:31:17 +0400

    - Fix SearchBar.tsx & Filters.tsx alignment

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -5,14 +5,16 @@ import ThemeToggle from "./ThemeToggle";
 export default function NavBar() {
   return (
     <div className="flex flex-col w-full">
-    <div className="flex w-full gap-15 items-center justify-between mt-10">
-        <h1 className="text-2xl font-main text-black dark:text-white">TODO LIST</h1>
-        <ThemeToggle/>
+      <div className="flex w-full gap-15 items-center justify-between mt-10">
+        <h1 className="text-2xl font-main text-black dark:text-white">
+          TODO LIST
+        </h1>
+        <ThemeToggle />
+      </div>
+      <div className="flex w-full gap-5 items-center justify-between">
+        <SearchBar></SearchBar>
+        <Filters></Filters>
+      </div>
     </div>
-    <div className="flex w-full gap-5 items-baseline justify-between mt-6">
-      <SearchBar></SearchBar>
-      <Filters></Filters>
-    </div>
-    </div>
-  )
+  );
 }
--- src/components/Todo/TodoList.tsx
+++ src/components/Todo/TodoList.tsx
@@ -8,7 +8,7 @@ export default function TodoList() {
   const filteredBySearchTodos = todos.filter((todo) => searchValue === '' ? true : todo.title.includes(searchValue))
   const filteredByFilterTodos = filteredBySearchTodos.filter((todo) => filterValue === 'all' ? true : filterValue === 'completed' ? todo.completed : !todo.completed)
   return (
-    <div className="flex w-full flex-col gap-5 pt-15">
+    <div className="flex w-full flex-col gap-5 pt-10">
         {
           filteredByFilterTodos.map((todo, index) => (
             <TodoItem todo={todo} key={index}/>
