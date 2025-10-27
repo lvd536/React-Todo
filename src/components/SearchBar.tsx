6d124288a97293e69a567922b2edccf14f26c76f
lvd. <nbikiti@gmail.com>
2025-10-27 22:49:39 +0400

    - dev: Test search bar implementation

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,8 +1,19 @@
+import { useState } from "react";
+import { setSearchValue } from "../store/useTodoStore";
+
 export default function SearchBar() {
+  const [search, setSearch] = useState<string>('');
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  }
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setSearchValue(search.toLowerCase());
+  }
   return (
     <>
-        <form className="font-second flex items-center w-full h-4 justify-between gap-5 mt-6" action="">
-          <input type="search" name="" id="" className="w-full h-full ring-indigo-500 dark:ring-white ring-1 rounded-xs p-4 text-xs text-gray-500"/>
+        <form className="font-second flex items-center w-full h-4 justify-between gap-5 mt-6" action="" onSubmit={handleSubmit}>
+          <input type="search" name="" id="" className="w-full h-full ring-indigo-500 dark:ring-white ring-1 rounded-xs p-4 text-xs text-gray-500" value={search} onChange={handleChange}/>
           <button type="submit" className="bg-indigo-500 rounded-xs w-34 h-9">Search</button>
         </form>
     </>
--- src/components/Todo/TodoList.tsx
+++ src/components/Todo/TodoList.tsx
@@ -1,12 +1,16 @@
-import { useTodos } from "../../store/useTodoStore";
+import { useFilter, useSearchValue, useTodos } from "../../store/useTodoStore";
 import TodoItem from "./TodoItem";
 
 export default function TodoList() {
   const todos = useTodos();
+  const searchValue = useSearchValue();
+  const filterValue = useFilter();
+  const filteredBySearchTodos = todos.filter((todo) => searchValue === '' ? true : todo.title.includes(searchValue))
+  const filteredByFilterTodos = filteredBySearchTodos.filter((todo) => filterValue === 'all' ? true : filterValue === 'completed' ? todo.completed : !todo.completed)
   return (
     <div className="flex w-full flex-col gap-5 pt-15">
         {
-          todos.map((todo, index) => (
+          filteredByFilterTodos.map((todo, index) => (
             <TodoItem todo={todo} key={index}/>
           ))
         }
