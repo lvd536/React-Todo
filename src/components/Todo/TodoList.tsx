c0ad9821156e0443e5e32a746df1192093383a1c
lvd. <nbikiti@gmail.com>
2025-10-28 21:36:48 +0400

    - Reduced todo sort &  render logic

--- src/components/Todo/TodoList.tsx
+++ src/components/Todo/TodoList.tsx
@@ -1,16 +1,22 @@
-import { useFilter, useSearchValue, useTodos } from "../../store/useTodoStore";
+import { useFilter, useSearchValue, useTodos, type ITodo } from "../../store/useTodoStore";
 import TodoItem from "./TodoItem";
 
 export default function TodoList() {
   const todos = useTodos();
   const searchValue = useSearchValue();
   const filterValue = useFilter();
-  const filteredBySearchTodos = todos.filter((todo) => searchValue === '' ? true : todo.title.includes(searchValue))
-  const filteredByFilterTodos = filteredBySearchTodos.filter((todo) => filterValue === 'all' ? true : filterValue === 'completed' ? todo.completed : !todo.completed)
+  
+  const getFilteredTodos = (todos: ITodo[], searchValue: string, filterValue: "all" | "completed" | "active") => {
+    const filteredBySearchTodos = todos.filter((todo) => searchValue === '' ? true : todo.title.includes(searchValue))
+    return filteredBySearchTodos.filter((todo) => filterValue === 'all' ? true : filterValue === 'completed' ? todo.completed : !todo.completed)
+  }
+
+  const filteredTodos = getFilteredTodos(todos, searchValue, filterValue);
+
   return (
     <div className="flex w-full flex-col gap-5 pt-10">
         {
-          filteredByFilterTodos.map((todo, index) => (
+          filteredTodos.map((todo, index) => (
             <TodoItem todo={todo} key={index}/>
           ))
         }
